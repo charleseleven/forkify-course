7cdcef8685da4febe26642d957aff48df105d982
charleseleven <cpiresrodrigues81@gmail.com>
2020-12-29 16:33:04 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a4e49041-b6db-4ee5-93e6-27d04e06511c';
+export const MODAL_CLOSE_SEC = 2.5;
